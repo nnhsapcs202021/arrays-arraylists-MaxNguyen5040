462d2cd5393b18649ee444d94a7b56b4411c2313
MaxNguyen5040 <MaxNguyen5040>
2021-01-25 14:24:42 -0600

    Started arraynotes

--- Strategy.java
+++ Strategy.java
@@ -1,6 +1,6 @@
-// Chomp strategy
-public interface Strategy
-{
-  Location findBestMove(ChompGame game);
-  Location findRandomMove(ChompGame game);
-}
+//// Chomp strategy
+//public interface Strategy
+//{
+//  Location findBestMove(ChompGame game);
+//  Location findRandomMove(ChompGame game);
+//}
